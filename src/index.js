51e7c1d5d82fd5c5adfadd285a25d4e4cd4f131b
SathishNagarajan <nsathishkumarct@gmail.com>
2024-03-04 16:11:42 +0530

    removed Index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-let excelJsConvertion = require('./excelJsConvertion');
-
-let sampleJsonDefault = require('./sampleJsonDefault');
-let sampleJsonCustom = require('./sampleJsonCustom');
-let sampleJsonTree = require('./sampleJsonTree');
-
-
-excelJsConvertion.initProcess(sampleJsonTree.config, sampleJsonTree.data);
